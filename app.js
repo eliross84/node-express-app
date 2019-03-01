95e2d03f54627e30705f57958e771924dc3db943
eliross84 <eliross84@gmail.com>
2019-03-01 11:29:39 -0600

    Added html file to display information

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const hostname = '127.0.0.1'   // set constants
 const port = 3002
 
 app.get('/', function (req, res) {
-  res.send('Welcome home!')
-})
-
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/big',  (req, res) =>{
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/greeting/:id',  (req, res) =>{
-  res.send('Hello! The id was ' + req.params.id)
-})
-
-app.get('/yo/:buddy',  (req, res) =>{
-  res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
+  res.sendFile(path.join(__dirname + '/html/index.html'))
 })
 
 // handle non-existant routes
