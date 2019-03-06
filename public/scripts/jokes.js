87da22e4326f0034b40e57565aad49609467a083
eliross84 <eliross84@gmail.com>
2019-03-05 19:18:08 -0600

    Adding the basis to button from script file.

---
+++ public/scripts/jokes.js
@@ -0,0 +1,4 @@
+const jokes = ["Programmers are like playwright's, computer's are just really bad actors.", "What is a IT Professional's birth control? ... Their personality", "What do you call 8 Hobbit's? ... A Hobbyte", "Which computer sings the best? ... A-Dell", "What do your air conditioner and computer have in common? ... They both are inefficient when Windows is open"];
+
+
+
