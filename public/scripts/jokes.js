ef7cf49522c82ff6d1e6aaba53b911893cbf4fc6
eliross84 <eliross84@gmail.com>
2019-03-05 19:49:37 -0600

    Adding joke button.

--- public/scripts/jokes.js
+++ public/scripts/jokes.js
@@ -1,4 +1,13 @@
-const jokes = ["Programmers are like playwright's, computer's are just really bad actors.", "What is a IT Professional's birth control? ... Their personality", "What do you call 8 Hobbit's? ... A Hobbyte", "Which computer sings the best? ... A-Dell", "What do your air conditioner and computer have in common? ... They both are inefficient when Windows is open"];
+const jokes = ['Programmers are like playwright\'s, computer\'s are just really bad actors.', 
+'What is a IT Professional\'s birth control? ... Their personality', 
+'What do you call 8 Hobbit\'s? ... A Hobbyte', 
+'Which computer sings the best? ... A-Dell', 
+'What do your air conditioner and computer have in common? ... They both are inefficient when Windows is open'];
+
+function newJoke(){
+    const random = Math.floor(Math.random() * (jokes.length));
+    document.getElementById('jokeDisplay').innerHTML = jokes[random];
+}
 
 
 
